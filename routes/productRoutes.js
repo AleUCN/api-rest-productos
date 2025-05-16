4f099a8df232406b62d47157747b72c5d8c5ddf4
AleUCN <alexander.estay@alumnos.ucn.cl>
2025-05-15 20:54:46 -0400

    feat: add authentication middleware to product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,11 +9,12 @@ const {
   deleteProduct
 } = require('../controllers/productController');
 
+const auth = require('../middlewares/authMiddleware');
 
-router.post('/', createProduct);
-router.get('/', getPaginatedProducts);
-router.get('/:uuid', getProductByUuid);
-router.patch('/:uuid', updateProduct);
-router.delete('/:uuid', deleteProduct);
+router.post('/', auth, createProduct);
+router.get('/', auth, getPaginatedProducts);
+router.get('/:uuid', auth, getProductByUuid);
+router.patch('/:uuid', auth, updateProduct);
+router.delete('/:uuid', auth, deleteProduct);
 
 module.exports = router;
